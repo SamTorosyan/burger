8976bcc98700fcdce8d810e38e09964aac8624d6
Sam Torosyan <samtorosyan@gmail.com>
2018-06-09 20:39:51 -0700

    sdfadsF

---
+++ DB/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burger (name) VALUES ('Vegi Burger');
+INSERT INTO burger (name) VALUES ('Beyound Burger');
+INSERT INTO burger (name) VALUES ('Black Bean Burger');
+INSERT INTO burger (name) VALUES ('Imposible Burger');
+INSERT INTO burger (name) VALUES ('Mushroom Burger');
+
+INSERT INTO burger (name) VALUES (true);
+INSERT INTO burger (name) VALUES (true);
+INSERT INTO burger (name) VALUES (true);
+INSERT INTO burger (name) VALUES (true);
+INSERT INTO burger (name) VALUES (true);
